2d5dc5939d0f5f0bfc7816f64461723b53f7f107
Diego Castillo <diegocasmo@gmail.com>
2019-01-27 18:13:34 +0100

    Fixed 'dotenv' initialization

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
+# Load environmental variables
+import os
 from dotenv import load_dotenv
-load_dotenv() # Load environmental variables
+load_dotenv(dotenv_path="%s/.env" % os.getcwd())
 
+# Load application
 from ml_blink_api import app
-
 if __name__ == '__main__':
   app.run()
