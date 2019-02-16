c035c470fb138637ee5db1231c7f36ef7fa1065f
Diego Castillo <diegocasmo@gmail.com>
2019-02-16 14:12:52 +0100

    Start celery app

--- ml_blink_api/config/celery_config.py
+++ ml_blink_api/config/celery_config.py
@@ -4,3 +4,6 @@ from celery import Celery
 celery = Celery('tasks',
   broker = os.getenv('CELERY_BROKER_URL'),
   backend = os.getenv('CELERY_RESULT_BACKEND'))
+
+if __name__ == '__main__':
+  celery.start()
